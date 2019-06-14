cde287f1ad7f817d7d6cca9194adfd421f647ade
Camilla <camillamv>
2019-06-13 23:16:58 -0300

    chore: add brazilian portuguese option for locale and make it default

---
+++ src/locales/pt-BR.js
@@ -0,0 +1,24 @@
+import antdData from 'antd/lib/locale-provider/pt_BR'
+import localeData from 'react-intl/locale-data/pt'
+
+const messages = {
+  'topBar.issuesHistory': 'Issues History',
+  'topBar.projectManagement': 'Project Management',
+  'topBar.typeToSearch': 'Type to search...',
+  'topBar.buyNow': 'Buy Now $24',
+  'topBar.bitcoin': 'Bitcoin',
+  'topBar.profileMenu.hello': 'Olá',
+  'topBar.profileMenu.billingPlan': 'Billing Plan',
+  'topBar.profileMenu.role': 'Role',
+  'topBar.profileMenu.email': 'E-mail',
+  'topBar.profileMenu.phone': 'Phone',
+  'topBar.profileMenu.editProfile': 'Editar perfil',
+  'topBar.profileMenu.logout': 'Sair',
+}
+
+export default {
+  locale: 'pt-BR',
+  antdData,
+  localeData,
+  messages,
+}
