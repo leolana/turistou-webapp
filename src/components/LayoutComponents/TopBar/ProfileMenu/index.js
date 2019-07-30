3b51c09ee4b7107711ca7e751a1b9742de30d6b3
Camilla <camillamv>
2019-07-30 08:11:23 -0300

    feat: add informations into dropdown menu profile and change icons

--- src/components/LayoutComponents/TopBar/ProfileMenu/index.js
+++ src/components/LayoutComponents/TopBar/ProfileMenu/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Menu, Dropdown, Avatar, Badge } from 'antd'
 import { FormattedMessage } from 'react-intl'
+import { getIdToken } from 'core/auth'
+import JwtDecode from 'jwt-decode'
 import styles from './style.module.scss'
 
 @connect(({ user }) => ({ user }))
@@ -20,11 +22,16 @@ class ProfileMenu extends React.Component {
   render() {
     const { user } = this.props
     const { count } = this.state
+    const token = getIdToken()
+    const info = JwtDecode(token)
+    console.log('\n\ninfo:', info)
+    const { nickname, email } = JwtDecode(token)
+
     const menu = (
       <Menu selectable={false}>
         <Menu.Item>
           <strong>
-            <FormattedMessage id="topBar.profileMenu.hello" />, {user.name || 'Anonymous'}
+            <FormattedMessage id="topBar.profileMenu.hello" />, {nickname || 'Anonymous'}
           </strong>
           {/* <div>
             <strong className="mr-1">
@@ -43,7 +50,7 @@ class ProfileMenu extends React.Component {
         <Menu.Item>
           <div>
             <strong>
-              <FormattedMessage id="topBar.profileMenu.email" />:{' '}
+              <FormattedMessage id="topBar.profileMenu.email" />:{email || ''}
             </strong>
             {user.email}
             {/* <br />
@@ -56,14 +63,14 @@ class ProfileMenu extends React.Component {
         <Menu.Divider />
         <Menu.Item>
           <a href="javascript: void(0);">
-            <i className={`${styles.menuIcon} icmn-user`} />
+            <i className={`${styles.menuIcon} fa fa-user-o`} />
             <FormattedMessage id="topBar.profileMenu.editProfile" />
           </a>
         </Menu.Item>
         <Menu.Divider />
         <Menu.Item>
           <a href="javascript: void(0);" onClick={this.logout}>
-            <i className={`${styles.menuIcon} icmn-exit`} />
+            <i className={`${styles.menuIcon} fa fa-sign-out`} />
             <FormattedMessage id="topBar.profileMenu.logout" />
           </a>
         </Menu.Item>
