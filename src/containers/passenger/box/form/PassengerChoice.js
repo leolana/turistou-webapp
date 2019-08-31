d6337e38b0da093b1a81f6de7cea4f620f100d82
Camilla <camillamv>
2019-08-31 12:25:16 -0300

    feat: add filter on client select option of new passenger

--- src/containers/passenger/box/form/PassengerChoice.js
+++ src/containers/passenger/box/form/PassengerChoice.js
@@ -38,7 +38,18 @@ class PassengerChoice extends Component {
               {form.getFieldDecorator('customer', {
                 rules: [{ required: false }],
               })(
-                <Select size="default">
+                <Select
+                  size="default"
+                  showSearch
+                  filterOption={(q, option) =>
+                    q.split(' ').every(x =>
+                      option.props.children
+                        .join('')
+                        .toLowerCase()
+                        .includes(x),
+                    )
+                  }
+                >
                   {tableData.map(x => (
                     <Select.Option key={x.id} value={x.id}>
                       {x.name} - {x.rg} - {x.city}
