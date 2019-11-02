bfbfcb7c9468a5b922020d15aef267bc1ab8180d
Leonardo Lana <leonardo.lana@itlab.com.br>
2019-11-02 11:39:19 -0300

    fix: path to excursion list index

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -3,7 +3,7 @@ import user from './user/sagas'
 import menu from './menu/sagas'
 import settings from './settings/sagas'
 import customer from './customer/sagas'
-import excursion from './excursion/sagas'
+import excursion from './excursionList/sagas'
 
 export default function* rootSaga() {
   yield all([user(), menu(), settings(), customer(), excursion()])
