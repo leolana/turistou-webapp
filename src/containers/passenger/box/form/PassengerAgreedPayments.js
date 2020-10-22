3ea1ee753f54e1f507476aac146266ae1aa13b91
Camilla <camilla.vianna@goomer.com.br>
2020-10-22 20:32:05 -0300

    feat: show total and missing agreed values

--- src/containers/passenger/box/form/PassengerAgreedPayments.js
+++ src/containers/passenger/box/form/PassengerAgreedPayments.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Form, Row, Col, Button } from 'antd'
 
@@ -6,6 +6,7 @@ import AgreedPayment from './AgreedPayment'
 
 const PassengerAgreedPayments = ({ form }) => {
   const { customerName, ticket } = useSelector(state => state.passengerDetail)
+  console.debug('ticket', ticket)
 
   const [conditionLastId, setConditionLastId] = useState(1)
 
@@ -14,18 +15,25 @@ const PassengerAgreedPayments = ({ form }) => {
 
     const keys = form.getFieldValue('keys')
     const nextKeys = keys.concat(conditionLastId)
-    form.setFieldsValue({
-      keys: nextKeys,
-    })
+    form.setFieldsValue({ keys: nextKeys })
   }, [conditionLastId, setConditionLastId, form])
 
   const handleRemovePayment = useCallback((k) => {
     const keys = form.getFieldValue('keys')
     if (keys.length === 1) return
 
-    form.setFieldsValue({
-      keys: keys.filter(key => key !== k),
-    })
+    form.setFieldsValue({ keys: keys.filter(key => key !== k) })
+  }, [form])
+
+  const paymentTotal = useMemo(() => {
+    const paymentConditions = form.getFieldValue('paymentConditions')
+
+    if (paymentConditions?.reduce)
+      return paymentConditions.reduce((total, payment) => {
+        return total + (payment?.value || 0)
+      }, 0)
+
+    return 0
   }, [form])
 
   form.getFieldDecorator('keys', { initialValue: [conditionLastId] })
@@ -45,12 +53,16 @@ const PassengerAgreedPayments = ({ form }) => {
       </Row>
 
       {keys.map((k, i) => (
-        <Form.Item key={`payment-condition--${i}`}>
+        <Form.Item key={`payment-condition--${k}`}>
           {form.getFieldDecorator(`paymentConditions[${i}]`, {
             initialValue: {},
           })(<AgreedPayment onRemove={() => handleRemovePayment(k)} />)}
         </Form.Item>
       ))}
+
+      Valor total: R$ {paymentTotal.toFixed(2)}
+      <br />
+      Valor em aberto / faltante: R$ {(ticket.price - paymentTotal).toFixed(2)}
 
       <Row type="flex" justify="center">
         <Col xs={16} md={8}>
