0fac4d84f68b428ee70f46ec35a9fd9ebda240f2
Camilla <camilla.vianna@goomer.com.br>
2020-12-05 11:36:36 -0300

    feat: delete excursion

--- src/redux/excursionList/actions.js
+++ src/redux/excursionList/actions.js
@@ -1,13 +1,14 @@
 import gql from 'graphql-tag'
 
-import { query } from 'core/api/apollo'
+import { query, mutate } from 'core/api/apollo'
 
 const actions = {
   SET_STATE: 'excursionList/SET_STATE',
   GET_EXCURSIONS: 'excursionList/GET_DATA',
   GET_EXCURSIONS_FAILURE: 'excursionList/GET_EXCURSIONS_FAILURE',
-  DELETE_DATA: 'excursionList/DELETE_DATA',
-  DELETE_DATA_SUCCESS: 'excursionList/DELETE_DATA_SUCCESS',
+  DELETE_EXCURSION: 'excursionList/DELETE_DATA',
+  DELETE_EXCURSION_SUCCESS: 'excursionList/DELETE_DATA_SUCCESS',
+  DELETE_EXCURSION_FAILURE: 'excursionList/DELETE_DATA_FAILURE',
 }
 
 const excursionFragment = gql`
@@ -67,4 +68,35 @@ export const fetchExcursionsFailure = () => ({
     payload: [],
   },
 })
+
+export const deleteExcursion = (id) => ({
+  type: actions.DELETE_EXCURSION,
+  payload: { loading: true },
+  request: () =>
+    mutate({
+      mutation: gql`
+        mutation deleteExcursion($id: String!) {
+          deleteExcursion(id: $id)
+        }
+      `,
+      variables: {
+        id,
+      },
+    }),
+})
+
+export const deleteExcursionsSuccess = (id) => ({
+  type: actions.DELETE_EXCURSION_SUCCESS,
+  payload: {
+    id,
+  },
+})
+
+export const deleteExcursionsFailure = () => ({
+  type: actions.DELETE_EXCURSIONS_FAILURE,
+  payload: {
+    isLoading: false,
+  },
+})
+
 export default actions
--- src/redux/excursionList/sagas.js
+++ src/redux/excursionList/sagas.js
@@ -1,15 +1,32 @@
 import { all, put, takeEvery, call } from 'redux-saga/effects'
 
-import actions, { fetchExcursions, fetchExcursionsSuccess, fetchExcursionsFailure } from './actions'
+import actions, {
+  fetchExcursionsSuccess,
+  fetchExcursionsFailure,
+  deleteExcursionsSuccess,
+  deleteExcursionsFailure,
+} from './actions'
 
-export function* getData() {
-  const fetchExcursion = fetchExcursions()
-  const result = yield call(fetchExcursion.request)
+export function* getData({ request }) {
+  const result = yield call(request)
 
   if (result.response && result.response.data) {
     yield put(fetchExcursionsSuccess(result.response.data))
   } else {
     yield put(fetchExcursionsFailure())
+    const error = result.error && result.error.networkError.result
+    const validationError = error && error.errors[0]
+    throw new Error(validationError)
+  }
+}
+
+export function* deleteData({ request }) {
+  const result = yield call(request)
+
+  if (result.response && result.response.data) {
+    yield put(deleteExcursionsSuccess(result.response.data.deleteExcursion))
+  } else {
+    yield put(deleteExcursionsFailure())
     const error = result.error && result.error.networkError.result
     const validationError = error && error.errors[0]
     throw new Error(validationError)
@@ -25,6 +42,7 @@ export function* SET_STATE() {
 export default function* rootSaga() {
   yield all([
     takeEvery(actions.GET_EXCURSIONS, getData),
+    takeEvery(actions.DELETE_EXCURSION, deleteData),
     SET_STATE(), // run once on app load to init listeners
   ])
 }
