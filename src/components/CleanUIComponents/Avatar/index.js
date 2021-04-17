040ab53baa993654645abecf0d20f4273e6053c9
camilla <CamillaMV@users.noreply.github.com>
2021-04-17 18:22:39 -0300

    fix: build dependencies

--- src/components/CleanUIComponents/Avatar/index.js
+++ src/components/CleanUIComponents/Avatar/index.js
@@ -1,30 +1,19 @@
 import React from 'react'
 import style from './style.module.scss'
 
-class Avatar extends React.Component {
-  static defaultProps = {
-    size: false,
-    border: false,
-    borderColor: '#d2d9e5',
-    src: '',
-  }
-
-  render() {
-    const { size, borderColor, src, border } = this.props
-    return (
-      <a
-        className={`${style.avatar} ${size ? style[`size${size}`] : ''} ${
-          border ? style.border : ''
-        }`}
-        href="javascript: void(0);"
-        style={{
-          borderColor,
-        }}
-      >
-        <img src={src} alt="User" />
-      </a>
-    )
-  }
+const Avatar = (props) => {
+  const { size = false, borderColor = '#d2d9e5', src, border = false } = props
+  return (
+    <a
+      className={`${style.avatar} ${size ? style[`size${size}`] : ''} ${
+        border ? style.border : ''
+      }`}
+      href="javascript: void(0);"
+      style={{ borderColor }}
+    >
+      <img src={src} alt="User" />
+    </a>
+  )
 }
 
 export default Avatar
