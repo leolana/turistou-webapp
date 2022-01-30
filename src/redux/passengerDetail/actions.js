44ef89893193e18fd1bf2dfe115cceaf9d024053
Camilla <camillamv@gmail.com>
2022-01-30 19:05:37 -0300

    refactor: clear sagas

--- src/redux/passengerDetail/actions.js
+++ src/redux/passengerDetail/actions.js
@@ -4,8 +4,6 @@ import gql from 'graphql-tag'
 const actions = {
   SET_STATE: 'passengerDetail/SET_STATE',
   SET_PAYLOAD: 'passengerDetail/SET_PAYLOAD',
-  SAVE_PASSENGER_FAILURE: 'passengerDetail/SAVE_PASSENGER_FAILURE',
-  SAVE_PASSENGER_SUCCESS: 'passengerDetail/SAVE_PASSENGER_SUCCESS',
 }
 
 export const SAVE_PASSENGER = gql`
@@ -15,17 +13,5 @@ export const SAVE_PASSENGER = gql`
     }
   }
 `
-
-export const savePassengerSuccess = (payload: any) => ({
-  payload: { ...payload },
-  type: actions.SET_STATE,
-  isLoading: false,
-})
-
-export const savePassengerFailure = (payload: any) => ({
-  type: actions.SAVE_PASSENGER_FAILURE,
-  payload: { ...payload },
-  isLoading: false,
-})
 
 export default actions
--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -2,25 +2,10 @@ import { all } from 'redux-saga/effects'
 import user from './user/sagas'
 import menu from './menu/sagas'
 import settings from './settings/sagas'
-import customerDetail from './customerDetail/sagas'
-import excursionList from './excursionList/sagas'
-import excursionDetail from './excursionDetail/sagas'
-import passengerList from './passengerList/sagas'
 import passengerStatus from './passengerStatus/sagas'
 import payments from './payments/sagas'
 import paymentForm from './paymentStatus/sagas'
 
 export default function* rootSaga() {
-  yield all([
-    user(),
-    menu(),
-    settings(),
-    customerDetail(),
-    excursionList(),
-    excursionDetail(),
-    passengerList(),
-    passengerStatus(),
-    payments(),
-    paymentForm(),
-  ])
+  yield all([user(), menu(), settings(), passengerStatus(), payments(), paymentForm()])
 }
